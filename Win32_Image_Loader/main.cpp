c462cb1f96de3dbdfa78931afd5768d5cc396612
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-10-31 12:22:35 +1100

    Add files via upload

---
+++ Win32_Image_Loader/main.cpp
@@ -0,0 +1,351 @@
+#include <Windows.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cstring>
+#include "resource.h"
+
+using namespace std;
+
+#define WINDOW_CLASS_NAME L"MultiThreaded Loader Tool"
+const unsigned int _kuiWINDOWWIDTH = 1280;
+const unsigned int _kuiWINDOWHEIGHT = 720;
+#define MAX_FILES_TO_OPEN 50
+#define MAX_CHARACTERS_IN_FILENAME 25
+
+//Global Variables
+std::vector<std::wstring> g_vecImageFileNames;
+std::vector<std::wstring> g_vecSoundFileNames;
+HINSTANCE g_hInstance;
+bool g_bIsFileLoaded = false;
+HBITMAP hBitMap = NULL;
+
+bool ChooseImageFilesToLoad(HWND _hwnd)
+{
+	OPENFILENAME ofn;
+	SecureZeroMemory(&ofn, sizeof(OPENFILENAME)); // Better to use than ZeroMemory
+	wchar_t wsFileNames[MAX_FILES_TO_OPEN * MAX_CHARACTERS_IN_FILENAME + MAX_PATH]; //The string to store all the filenames selected in one buffer togther with the complete path name.
+	wchar_t _wsPathName[MAX_PATH + 1];
+	wchar_t _wstempFile[MAX_PATH + MAX_CHARACTERS_IN_FILENAME]; //Assuming that the filename is not more than 20 characters
+	wchar_t _wsFileToOpen[MAX_PATH + MAX_CHARACTERS_IN_FILENAME];
+	ZeroMemory(wsFileNames, sizeof(wsFileNames));
+	ZeroMemory(_wsPathName, sizeof(_wsPathName));
+	ZeroMemory(_wstempFile, sizeof(_wstempFile));
+
+	//Fill out the fields of the structure
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = _hwnd;
+	ofn.lpstrFile = wsFileNames;
+	ofn.nMaxFile = MAX_FILES_TO_OPEN * 20 + MAX_PATH;  //The size, in charactesr of the buffer pointed to by lpstrFile. The buffer must be atleast 256(MAX_PATH) characters long; otherwise GetOpenFileName and 
+													   //GetSaveFileName functions return False
+													   // Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+													   // use the contents of wsFileNames to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.lpstrFilter = L"Bitmap Images(.bmp)\0*.bmp\0"; //Filter for bitmap images
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_FILEMUSTEXIST | OFN_ALLOWMULTISELECT;
+
+	//If the user makes a selection from the  open dialog box, the API call returns a non-zero value
+	if (GetOpenFileName(&ofn) != 0) //user made a selection and pressed the OK button
+	{
+		//Extract the path name from the wide string -  two ways of doing it
+		//First way: just work with wide char arrays
+		wcsncpy_s(_wsPathName, wsFileNames, ofn.nFileOffset);
+		int i = ofn.nFileOffset;
+		int j = 0;
+
+		while (true)
+		{
+			if (*(wsFileNames + i) == '\0')
+			{
+				_wstempFile[j] = *(wsFileNames + i);
+				wcscpy_s(_wsFileToOpen, _wsPathName);
+				wcscat_s(_wsFileToOpen, L"\\");
+				wcscat_s(_wsFileToOpen, _wstempFile);
+				g_vecImageFileNames.push_back(_wsFileToOpen);
+				j = 0;
+			}
+			else
+			{
+				_wstempFile[j] = *(wsFileNames + i);
+				j++;
+			}
+			if (*(wsFileNames + i) == '\0' && *(wsFileNames + i + 1) == '\0')
+			{
+				break;
+			}
+			else
+			{
+				i++;
+			}
+
+		}
+
+		g_bIsFileLoaded = true;
+		return true;
+	}
+	else // user pressed the cancel button or closed the dialog box or an error occured
+	{
+		return false;
+	}
+
+}
+
+bool ChooseSoundFilesToLoad(HWND _hwnd)
+{
+	OPENFILENAME ofn;
+	SecureZeroMemory(&ofn, sizeof(OPENFILENAME)); // Better to use than ZeroMemory
+	wchar_t wsFileNames[MAX_FILES_TO_OPEN * MAX_CHARACTERS_IN_FILENAME + MAX_PATH]; //The string to store all the filenames selected in one buffer togther with the complete path name.
+	wchar_t _wsPathName[MAX_PATH + 1];
+	wchar_t _wstempFile[MAX_PATH + MAX_CHARACTERS_IN_FILENAME]; //Assuming that the filename is not more than 20 characters
+	wchar_t _wsFileToOpen[MAX_PATH + MAX_CHARACTERS_IN_FILENAME];
+	ZeroMemory(wsFileNames, sizeof(wsFileNames));
+	ZeroMemory(_wsPathName, sizeof(_wsPathName));
+	ZeroMemory(_wstempFile, sizeof(_wstempFile));
+
+	//Fill out the fields of the structure
+	ofn.lStructSize = sizeof(OPENFILENAME);
+	ofn.hwndOwner = _hwnd;
+	ofn.lpstrFile = wsFileNames;
+	ofn.nMaxFile = MAX_FILES_TO_OPEN * 20 + MAX_PATH;  //The size, in charactesr of the buffer pointed to by lpstrFile. The buffer must be atleast 256(MAX_PATH) characters long; otherwise GetOpenFileName and 
+													   //GetSaveFileName functions return False
+													   // Set lpstrFile[0] to '\0' so that GetOpenFileName does not 
+													   // use the contents of wsFileNames to initialize itself.
+	ofn.lpstrFile[0] = '\0';
+	ofn.lpstrFilter = L"Wave Files (*.wav)\0*.wav\0All Files (*.*)\0*.*\0"; //Filter for wav files
+	ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_FILEMUSTEXIST | OFN_ALLOWMULTISELECT;
+
+	//If the user makes a selection from the  open dialog box, the API call returns a non-zero value
+	if (GetOpenFileName(&ofn) != 0) //user made a selection and pressed the OK button
+	{
+		//Extract the path name from the wide string -  two ways of doing it
+		//Second way: work with wide strings and a char pointer 
+
+		std::wstring _wstrPathName = ofn.lpstrFile;
+
+		_wstrPathName.resize(ofn.nFileOffset, '\\');
+
+		wchar_t *_pwcharNextFile = &ofn.lpstrFile[ofn.nFileOffset];
+
+		while (*_pwcharNextFile)
+		{
+			std::wstring _wstrFileName = _wstrPathName + _pwcharNextFile;
+
+			g_vecSoundFileNames.push_back(_wstrFileName);
+
+			_pwcharNextFile += lstrlenW(_pwcharNextFile) + 1;
+		}
+
+		g_bIsFileLoaded = true;
+		return true;
+	}
+	else // user pressed the cancel button or closed the dialog box or an error occured
+	{
+		return false;
+	}
+
+}
+
+LRESULT CALLBACK WindowProc(HWND _hwnd, UINT _uiMsg, WPARAM _wparam, LPARAM _lparam)
+{
+	PAINTSTRUCT ps;
+	HDC _hWindowDC;
+	BITMAP bitmap;
+	HDC hdcMem;
+	HGDIOBJ oldBitMap;
+	//RECT rect;
+	switch (_uiMsg)
+	{
+	case WM_KEYDOWN:
+	{
+		switch (_wparam)
+		{
+		case VK_ESCAPE:
+		{
+			SendMessage(_hwnd, WM_CLOSE, 0, 0);
+			return(0);
+		}
+		break;
+		default:
+			break;
+		}
+	}
+	break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(_wparam))
+		{
+
+
+		case ID_FILE_LOADIMAGE:
+		{
+			if (ChooseImageFilesToLoad(_hwnd))
+			{
+				bool doingWork = true;
+				//Write code here to create multiple threads to load image files in parallel
+				MessageBox(_hwnd, g_vecImageFileNames.back().c_str(), L"Success Message", MB_OK);
+				hBitMap = (HBITMAP)LoadImage(NULL, g_vecImageFileNames.back().c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+				//i got this from somewhere along the interwebs : doesn't display anything yet.... but also no errors
+				/*HDC hdc = CreateCompatibleDC(NULL);
+				HBITMAP bitmap = (HBITMAP)LoadImage(NULL, g_vecImageFileNames.front().c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+				SelectObject(hdc, bitmap);
+				while (doingWork)
+				{
+					HDC hdc_x = GetDC(HWND_DESKTOP);
+					BitBlt(hdc_x, 488, 359, 48, 48, hdc, 0, 0, SRCCOPY);
+					ReleaseDC(HWND_DESKTOP, hdc_x);
+					doingWork = false;
+				}*/
+
+				/*RECT getRekt;
+				HDC hdc = GetDC(_hwnd);
+				HBRUSH brush = CreatePatternBrush((HBITMAP)LoadImage(NULL, g_vecImageFileNames.front().c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE));
+				GetWindowRect(_hwnd, &getRekt);
+				FillRect(hdc, &getRekt, brush);
+				DeleteObject(brush);
+				ReleaseDC(_hwnd, hdc);*/
+			}
+			else
+			{
+				MessageBox(_hwnd, L"No Image File selected", L"Error Message", MB_ICONWARNING);
+			}
+
+			return (0);
+		}
+		break;
+		case ID_FILE_LOADSOUND:
+		{
+			if (ChooseSoundFilesToLoad(_hwnd))
+			{
+				//Write code here to create multiple threads to load sound files in parallel
+				MessageBox(_hwnd, L"A Sound File is selected", L"Success Message", MB_OK);
+			}
+			else
+			{
+				MessageBox(_hwnd, L"No Sound File selected", L"Error Message", MB_ICONWARNING);
+			}
+			return (0);
+		}
+		break;
+		case ID_EXIT:
+		{
+			SendMessage(_hwnd, WM_CLOSE, 0, 0);
+			return (0);
+		}
+		break;
+		default:
+			break;
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		_hWindowDC = BeginPaint(_hwnd, &ps);
+		//Do all our painting here
+		hdcMem = CreateCompatibleDC(_hWindowDC);
+		oldBitMap = SelectObject(hdcMem, hBitMap);
+
+		GetObject(hBitMap, sizeof(bitmap), &bitmap);
+		BitBlt(_hWindowDC, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
+		SelectObject(hdcMem, oldBitMap);
+		DeleteDC(hdcMem);
+
+		EndPaint(_hwnd, &ps);
+		return (0);
+	}
+	break;
+	case WM_CLOSE:
+	{
+		DeleteObject(hBitMap);
+		PostQuitMessage(0);
+	}
+	break;
+	default:
+		break;
+	}
+	return (DefWindowProc(_hwnd, _uiMsg, _wparam, _lparam));
+}
+
+
+HWND CreateAndRegisterWindow(HINSTANCE _hInstance)
+{
+	WNDCLASSEX winclass; // This will hold the class we create.
+	HWND hwnd;           // Generic window handle.
+
+						 // First fill in the window class structure.
+	winclass.cbSize = sizeof(WNDCLASSEX);
+	winclass.style = CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	winclass.lpfnWndProc = WindowProc;
+	winclass.cbClsExtra = 0;
+	winclass.cbWndExtra = 0;
+	winclass.hInstance = _hInstance;
+	winclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	winclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	winclass.hbrBackground =
+		static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+	winclass.lpszMenuName = NULL;
+	winclass.lpszClassName = WINDOW_CLASS_NAME;
+	winclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	// register the window class
+	if (!RegisterClassEx(&winclass))
+	{
+		return (0);
+	}
+
+	HMENU _hMenu = LoadMenu(_hInstance, MAKEINTRESOURCE(IDR_MENU1));
+
+	// create the window
+	hwnd = CreateWindowEx(NULL, // Extended style.
+		WINDOW_CLASS_NAME,      // Class.
+		L"MultiThreaded Loader Tool",   // Title.
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		10, 10,                    // Initial x,y.
+		_kuiWINDOWWIDTH, _kuiWINDOWHEIGHT,                // Initial width, height.
+		NULL,                   // Handle to parent.
+		_hMenu,                   // Handle to menu.
+		_hInstance,             // Instance of this application.
+		NULL);                  // Extra creation parameters.
+
+	return hwnd;
+}
+
+
+
+int WINAPI WinMain(HINSTANCE _hInstance,
+	HINSTANCE _hPrevInstance,
+	LPSTR _lpCmdLine,
+	int _nCmdShow)
+{
+	MSG msg;  //Generic Message
+
+	HWND _hwnd = CreateAndRegisterWindow(_hInstance);
+
+	if (!(_hwnd))
+	{
+		return (0);
+	}
+
+
+	// Enter main event loop
+	while (true)
+	{
+		// Test if there is a message in queue, if so get it.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			// Test if this is a quit.
+			if (msg.message == WM_QUIT)
+			{
+				break;
+			}
+
+			// Translate any accelerator keys.
+			TranslateMessage(&msg);
+			// Send the message to the window proc.
+			DispatchMessage(&msg);
+		}
+
+	}
+
+	// Return to Windows like this...
+	return (static_cast<int>(msg.wParam));
+}
